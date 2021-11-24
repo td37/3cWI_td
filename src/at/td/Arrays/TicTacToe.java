ede74260ab1dde2a0683c3dc4478208de9984440
Theresa Denz <tdenz@gmx.at>
2021-11-24 09:10:12 +0100

    TicTacToe win conditions optimized

--- src/at/td/Arrays/TicTacToe.java
+++ src/at/td/Arrays/TicTacToe.java
@@ -4,25 +4,20 @@ import java.util.Scanner;
 
 public class TicTacToe {
     private static int[][] gameField = new int[3][3];
-    private static boolean win = false;;
+    private static boolean win = false;
+    private static boolean tied = false;
+    private static boolean wonStop = false;
+    private static int gameRounds = 0;
+    ;
 
     public static void main(String[] args) {
         int input = 0;
 
 
-
-
         Scanner scanner = new Scanner(System.in);
 
 
-
         while (win == false) {
-
-            for (int i = 0; i < 10; i=+2) {
-                if (i == 9){
-                    System.out.println("Ou ein Unentschieden.");
-                }
-            }
 
             System.out.println("Willkommen bei TicTacToe. Vor jedem Zug wird dir das Spielfeld angezeigt!");
             System.out.println("Eine 0 ist ein leeres Feld, eine 1 steht für Spieler 1, 2 für Spieler 2.");
@@ -42,8 +37,16 @@ public class TicTacToe {
             int value2 = Integer.parseInt(result[1]);
 
             gameField[value1 - 1][value2 - 1] = 1;
-
+            gameRounds = gameRounds + 1;
+            tied();
             gameOver();
+            if (tied){
+                break;
+            }
+            if (wonStop){
+                break;
+            }
+
 
             for (int row = 0; row < gameField.length; row++) {
                 for (int col = 0; col < gameField.length; col++) {
@@ -60,175 +63,193 @@ public class TicTacToe {
             value2 = Integer.parseInt(result[1]);
 
             gameField[value1 - 1][value2 - 1] = 2;
-
+            gameRounds = gameRounds + 1;
+            tied();
             gameOver();
+            if (tied){
+                break;
+            }
+            if (wonStop){
+                break;
+            }
+
+        }
+
+
+    }
+
+    public static void tied() {
+        if (gameRounds == 9) {
+            System.out.println("Ou ein Unentschieden.");
+            tied = true;
+            return;
         }
     }
 
-        public static void gameOver() {
-            // Win conditions Player 1
-            // 1 Senkrecht
-            if (gameField[0][0] == 1) {
-                if (gameField[1][0] == 1) {
-                    if (gameField[2][0] == 1) {
-                        win = true;
-                        System.out.println("Spieler 1 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 1 Waagrecht
-            if (gameField[0][0] == 1) {
-                if (gameField[0][1] == 1) {
-                    if (gameField[0][2] == 1) {
-                        win = true;
-                        System.out.println("Spieler 1 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 2 Senkrecht
-            if (gameField[0][1] == 1) {
-                if (gameField[1][1] == 1) {
-                    if (gameField[2][1] == 1) {
-                        win = true;
-                        System.out.println("Spieler 1 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 2 Waagrecht
+    public static void gameOver() {
+        // Win conditions Player 1
+        // 1 Senkrecht
+        if (gameField[0][0] == 1) {
             if (gameField[1][0] == 1) {
-                if (gameField[1][1] == 1) {
-                    if (gameField[1][2] == 1) {
-                        win = true;
-                        System.out.println("Spieler 1 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 3 Senkrecht
-            if (gameField[0][2] == 1) {
-                if (gameField[1][2] == 1) {
-                    if (gameField[2][2] == 1) {
-                        win = true;
-                        System.out.println("Spieler 1 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 3 Waagrecht
-            if (gameField[2][0] == 1) {
-                if (gameField[2][1] == 1) {
-                    if (gameField[2][2] == 1) {
-                        win = true;
-                        System.out.println("Spieler 1 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 4 Diagonal
-            if (gameField[0][0] == 1) {
-                if (gameField[1][1] == 1) {
-                    if (gameField[2][2] == 1) {
-                        win = true;
-                        System.out.println("Spieler 1 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 4 Diagonal
-            if (gameField[0][2] == 1) {
-                if (gameField[1][1] == 1) {
-                    if (gameField[2][0] == 1) {
-                        win = true;
-                        System.out.println("Spieler 1 hat gewonnen!");
-                    }
-                }
-            }
-
-
-            // Win conditions Player 2
-            // 1 Senkrecht
-            if (gameField[0][0] == 2) {
-                if (gameField[1][0] == 2) {
-                    if (gameField[2][0] == 2) {
-                        win = true;
-                        System.out.println("Spieler 2 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 1 Waagrecht
-            if (gameField[0][0] == 2) {
-                if (gameField[0][1] == 2) {
-                    if (gameField[0][2] == 2) {
-                        win = true;
-                        System.out.println("Spieler 2 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 2 Senkrecht
-            if (gameField[0][1] == 2) {
-                if (gameField[1][1] == 2) {
-                    if (gameField[2][1] == 2) {
-                        win = true;
-                        System.out.println("Spieler 2 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 2 Waagrecht
-            if (gameField[1][0] == 2) {
-                if (gameField[1][1] == 2) {
-                    if (gameField[1][2] == 2) {
-                        win = true;
-                        System.out.println("Spieler 2 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 3 Senkrecht
-            if (gameField[0][2] == 2) {
-                if (gameField[1][2] == 2) {
-                    if (gameField[2][2] == 2) {
-                        win = true;
-                        System.out.println("Spieler 2 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 3 Waagrecht
-            if (gameField[2][0] == 2) {
-                if (gameField[2][1] == 2) {
-                    if (gameField[2][2] == 2) {
-                        win = true;
-                        System.out.println("Spieler 2 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 4 Diagonal
-            if (gameField[0][0] == 2) {
-                if (gameField[1][1] == 2) {
-                    if (gameField[2][2] == 2) {
-                        win = true;
-                        System.out.println("Spieler 2 hat gewonnen!");
-                    }
-                }
-            }
-
-            // 4 Diagonal
-            if (gameField[0][2] == 2) {
-                if (gameField[1][1] == 2) {
-                    if (gameField[2][0] == 2) {
-                        win = true;
-                        System.out.println("Spieler 2 hat gewonnen!");
-                    }
+                if (gameField[2][0] == 1) {
+                    wonStop = true;
+                    System.out.println("Spieler 1 hat gewonnen!");
                 }
             }
         }
+
+        // 1 Waagrecht
+        if (gameField[0][0] == 1) {
+            if (gameField[0][1] == 1) {
+                if (gameField[0][2] == 1) {
+                    wonStop = true;
+                    System.out.println("Spieler 1 hat gewonnen!");
+                }
+            }
+        }
+
+        // 2 Senkrecht
+        if (gameField[0][1] == 1) {
+            if (gameField[1][1] == 1) {
+                if (gameField[2][1] == 1) {
+                    wonStop = true;
+                    System.out.println("Spieler 1 hat gewonnen!");
+                }
+            }
+        }
+
+        // 2 Waagrecht
+        if (gameField[1][0] == 1) {
+            if (gameField[1][1] == 1) {
+                if (gameField[1][2] == 1) {
+                    wonStop = true;
+                    System.out.println("Spieler 1 hat gewonnen!");
+                }
+            }
+        }
+
+        // 3 Senkrecht
+        if (gameField[0][2] == 1) {
+            if (gameField[1][2] == 1) {
+                if (gameField[2][2] == 1) {
+                    wonStop = true;
+                    System.out.println("Spieler 1 hat gewonnen!");
+                }
+            }
+        }
+
+        // 3 Waagrecht
+        if (gameField[2][0] == 1) {
+            if (gameField[2][1] == 1) {
+                if (gameField[2][2] == 1) {
+                    wonStop = true;
+                    System.out.println("Spieler 1 hat gewonnen!");
+                }
+            }
+        }
+
+        // 4 Diagonal
+        if (gameField[0][0] == 1) {
+            if (gameField[1][1] == 1) {
+                if (gameField[2][2] == 1) {
+                    wonStop = true;
+                    System.out.println("Spieler 1 hat gewonnen!");
+                }
+            }
+        }
+
+        // 4 Diagonal
+        if (gameField[0][2] == 1) {
+            if (gameField[1][1] == 1) {
+                if (gameField[2][0] == 1) {
+                    wonStop = true;
+                    System.out.println("Spieler 1 hat gewonnen!");
+                }
+            }
+        }
+
+
+        // Win conditions Player 2
+        // 1 Senkrecht
+        if (gameField[0][0] == 2) {
+            if (gameField[1][0] == 2) {
+                if (gameField[2][0] == 2) {
+                    wonStop = true;
+                    System.out.println("Spieler 2 hat gewonnen!");
+                }
+            }
+        }
+
+        // 1 Waagrecht
+        if (gameField[0][0] == 2) {
+            if (gameField[0][1] == 2) {
+                if (gameField[0][2] == 2) {
+                    wonStop = true;
+                    System.out.println("Spieler 2 hat gewonnen!");
+                }
+            }
+        }
+
+        // 2 Senkrecht
+        if (gameField[0][1] == 2) {
+            if (gameField[1][1] == 2) {
+                if (gameField[2][1] == 2) {
+                    wonStop = true;
+                    System.out.println("Spieler 2 hat gewonnen!");
+                }
+            }
+        }
+
+        // 2 Waagrecht
+        if (gameField[1][0] == 2) {
+            if (gameField[1][1] == 2) {
+                if (gameField[1][2] == 2) {
+                    wonStop = true;
+                    System.out.println("Spieler 2 hat gewonnen!");
+                }
+            }
+        }
+
+        // 3 Senkrecht
+        if (gameField[0][2] == 2) {
+            if (gameField[1][2] == 2) {
+                if (gameField[2][2] == 2) {
+                    wonStop = true;
+                    System.out.println("Spieler 2 hat gewonnen!");
+                }
+            }
+        }
+
+        // 3 Waagrecht
+        if (gameField[2][0] == 2) {
+            if (gameField[2][1] == 2) {
+                if (gameField[2][2] == 2) {
+                    wonStop = true;
+                    System.out.println("Spieler 2 hat gewonnen!");
+                }
+            }
+        }
+
+        // 4 Diagonal
+        if (gameField[0][0] == 2) {
+            if (gameField[1][1] == 2) {
+                if (gameField[2][2] == 2) {
+                    wonStop = true;
+                    System.out.println("Spieler 2 hat gewonnen!");
+                }
+            }
+        }
+
+        // 4 Diagonal
+        if (gameField[0][2] == 2) {
+            if (gameField[1][1] == 2) {
+                if (gameField[2][0] == 2) {
+                    wonStop = true;
+                    System.out.println("Spieler 2 hat gewonnen!");
+                }
+            }
+        }
+    }
 
 
 }
