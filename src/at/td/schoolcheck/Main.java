b60341487cbabb6cbcff519c21093175770d2b80
Theresa Denz <tdenz@gmx.at>
2021-11-26 12:00:39 +0100

    schoolcheck nr 1

---
+++ src/at/td/schoolcheck/Main.java
@@ -0,0 +1,26 @@
+package at.td.schoolcheck;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Wie viel zufällige Sätze möchten sie erhalten? Bitte geben sie eine Zahl ein.");
+        int howManySentences = scanner.nextInt();
+
+        String[] person  = {"Er", "Sie"};
+        String[] verb ={"geht", "läuft", "rennt", "fährt"};
+        String[] place = {"nach Hause", "zur Schule", "ins Büro", "zur Party"};
+
+        for (int i = 0; i < howManySentences ; i++) {
+            int randomPerson = random.nextInt(2);
+            int randomVerb = random.nextInt(4);
+            int randomPlace = random.nextInt(4);
+
+            System.out.println(person[randomPerson] + " " + verb[randomVerb] + " " + place[randomPlace]);
+        }
+    }
+}
