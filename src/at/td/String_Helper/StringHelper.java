8b4cd1dedc7b5f7908986be7f5aafb15e35a3172
Theresa Denz <tdenz@gmx.at>
2021-12-01 09:21:56 +0100

    isPalindrom finished, restructured code, progress countLetters

--- src/at/td/String_Helper/StringHelper.java
+++ src/at/td/String_Helper/StringHelper.java
@@ -5,16 +5,35 @@ import java.util.Scanner;
 public class StringHelper {
 
     public static void main(String[] args) {
-        //StringHelper.getAmountOfLetters();
-        //StringHelper.reverseString();
+
+        //isPalindrome
         System.out.println("Bitte gib ein Wort ein, von dem du wissen möchtest, ob man es von vorne oder hinten lesen kann.");
         Scanner scanner = new Scanner(System.in);
-       String input = scanner.next();
+        String input = scanner.next();
 
-       boolean isPalindrome =isPalindrome("");
-       if (isPalindrome){
+        boolean isPalindrome = isPalindrome(input);
+        if (isPalindrome) {
+            System.out.println("Das Wort ist von vorne und hinten lesbar.");
+        } else {
+            System.out.println("Man kann das Wort nicht von vorne und hinten lesen.");
+        }
 
-       }else{}
+        //countLetters
+
+
+        //reverseString
+        System.out.println("Bitte gib ein Wort ein, welches dir dann rückwärts ausgegeben wird.");
+        Scanner wordForward = new Scanner(System.in);
+        String wordUserWantsBackwards = wordForward.next();
+        String reverseString = reverseString(wordUserWantsBackwards);
+        System.out.println("Ergebnis: " + reverseString);
+
+        //getAmountOfLetters
+        System.out.println("Bitte gib ein Wort ein um dir die Anzahl der Buchstaben ausgeben zu lassen.");
+        Scanner letters = new Scanner(System.in);
+        String wordAmountOfLetters = letters.next();
+        int resultAmountOfLetters = getAmountOfLetters(wordAmountOfLetters);
+        System.out.println(resultAmountOfLetters);
 
 
     }
@@ -34,50 +53,62 @@ public class StringHelper {
                     endWord = endWord - 1;
                 }
             }
-            System.out.println("Das Wort ist von vorne und hinten lesbar.");
+            return true;
         } else {
             for (int i = 0; i < (input.length() - 1) / 2; i++) {
                 if (word[startWord] == word[endWord]) {
                     startWord = startWord + 1;
                     endWord = endWord - 1;
                 } else {
-                    System.out.println("Das Wort kann man nicht von vorne und hinten lesen.");
                     return false;
                 }
-                System.out.println("Das Wort ist von vorne und hinten lesbar.");
             }
             return true;
         }
-
-
-        return false;
-
     }
 
     public static int countLetters(String text, char number) {
-        return -1;
-    }
-
-    public static String reverseString() {
         letters();
-        System.out.println("Bitte gib ein Wort ein welches dir dann rückwärts ausgegeben wird.");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char word[] = new char[input.length()];
-        word = input.toCharArray();
-        for (int i = input.length() - 1; i > -1; i--) {
-            System.out.print(word[i]);
+        System.out.println("Bitte gib ein Wort ein, von dem du wissen möchtest, von welchem Buchstabe wie viele vorhanden sind.");
+        Scanner countLetters = new Scanner(System.in);
+        String wordToCountLetters = countLetters.next();
+        char lettersFromWord[] = new char[wordToCountLetters.length()];
+        lettersFromWord = wordToCountLetters.toCharArray();
+
+        char[] letters = new char[26];
+
+        int lettersFromTheAlphabet = 0;
+
+        for (int i = 0; i <= 26; i++) {
+            for (int j = 0; j < ; j++) {
+                if (letters[i] = lettersFromWord[j]){
+                    letters[lettersFromTheAlphabet] = Character.valueOf(i);
+                    lettersFromTheAlphabet++;
+                }
+            }
+
+
         }
-        return "";
+
+        return -1;
     }
 
-    public static int getAmountOfLetters() {
-        System.out.println("Bitte gib ein Wort ein um dir die Anzahl der Buchstaben ausgeben zu lassen.");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char word[] = new char[input.length()];
-        System.out.println(input.length());
-        return -1;
+    public static String reverseString(String wordUserWantsBackwards) {
+        letters();
+
+        String wordBackwardsReturn = "";
+        char wordBackwards[] = new char[wordUserWantsBackwards.length()];
+        wordBackwards = wordUserWantsBackwards.toCharArray();
+        for (int i = wordUserWantsBackwards.length() - 1; i > -1; i--) {
+            wordBackwardsReturn = wordBackwardsReturn + wordBackwards[i];
+        }
+
+        return wordBackwardsReturn;
+    }
+
+    public static int getAmountOfLetters(String wordAmountOfLetters) {
+        char word[] = new char[wordAmountOfLetters.length()];
+        return wordAmountOfLetters.length();
     }
 
     public static void letters() {
