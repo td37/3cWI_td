b5245f89b88683a23a5ee0971a3ddf0d297d6c12
Theresa Denz <tdenz@gmx.at>
2021-12-17 12:11:56 +0100

    new class Tank

---
+++ src/at/td/ooEinstieg/firstObject/Tank.java
@@ -0,0 +1,17 @@
+package at.td.ooEinstieg.firstObject;
+
+public class Tank {
+    private int volume;
+
+    public Tank(int volume) {
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+}
