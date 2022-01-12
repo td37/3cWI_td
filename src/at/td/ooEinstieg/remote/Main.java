114b1bda2e8b1d4493319bc920e4e7175a66d5e4
Theresa Denz <tdenz@gmx.at>
2022-01-12 09:28:28 +0100

    created a remote with batteries

--- src/at/td/ooEinstieg/remote/Main.java
+++ src/at/td/ooEinstieg/remote/Main.java
@@ -1,11 +1,15 @@
 package at.td.ooEinstieg.remote;
 
 public class Main {
-    Battery b1 = new Battery(64);
-    Battery b2 = new Battery(82);
+    public static void main(String[] args) {
 
-    Remote r1 = new Remote();
 
-    r1.addBatteries(b1);
-    r1.addBatteries(b2);
+        Battery b1 = new Battery(64);
+        Battery b2 = new Battery(82);
+
+        Remote r1 = new Remote();
+
+        r1.addBatteries(b1);
+        r1.addBatteries(b2);
+    }
 }
