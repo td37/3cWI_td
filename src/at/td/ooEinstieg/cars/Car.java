2aefc6ce85f899d3d03a846f783373a047e2c5f4
Theresa Denz <tdenz@gmx.at>
2022-01-19 09:17:37 +0100

    added newBaseConsumption() function

--- src/at/td/ooEinstieg/cars/Car.java
+++ src/at/td/ooEinstieg/cars/Car.java
@@ -6,14 +6,20 @@ public class Car {
     private String color;
     private int maxSpeed;
     private int basePrice;
-    private int baseConsumption;
+    private double baseConsumption;
+    private int mileage;
 
-    public Car(Engine engine, Producer producer, String color, int maxSpeed, int basePrice, int baseConsumption) {
+    public Car(Engine engine, Producer producer, String color, int maxSpeed, int basePrice, double baseConsumption, int mileage) {
         this.engine = engine;
         this.producer = producer;
         this.color = color;
         this.maxSpeed = maxSpeed;
         this.basePrice = basePrice;
+        this.baseConsumption = baseConsumption;
+        this.mileage = mileage;
+    }
+
+    public void setBaseConsumption(double baseConsumption) {
         this.baseConsumption = baseConsumption;
     }
 
@@ -21,5 +27,14 @@ public class Car {
 
         double discountedPrice = this.basePrice - (this.basePrice * (this.producer.getDiscount()/100));
         System.out.println(discountedPrice);
+    }
+
+    public void newBaseComsumption(){
+        double newBaseConsumption;
+        if(this.mileage >= 50000){
+            newBaseConsumption = this.baseConsumption + (this.baseConsumption * 0.098);
+            setBaseConsumption(newBaseConsumption);
+            System.out.println(newBaseConsumption);
+        } else System.out.println("Alles oke, dein Auto verbaucht noch den Basisverbrauch");
     }
 }
