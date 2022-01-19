42bc455f87688e44aacb617c1cfc49cb24ed4d32
Theresa Denz <tdenz@gmx.at>
2022-01-19 09:33:17 +0100

    functions add(), subtract(), multiply(), divide()

--- src/at/td/ooEinstieg/Calculator/basicCalculator.java
+++ src/at/td/ooEinstieg/Calculator/basicCalculator.java
@@ -7,19 +7,23 @@ public class basicCalculator {
         this.color = color;
     }
 
-    public void add(){
-        System.out.println("Ich kann addieren.");
+    public void add(int nr1, int nr2){
+        int addedNumbers = nr1 + nr2;
+        System.out.println("Die Zahlen " + nr1 + " und " + nr2 + " ergeben addiert " + addedNumbers);
     }
 
-    public void subtract(){
-        System.out.println("Ich kann subtrahieren.");
+    public void subtract(int nr1, int nr2){
+        int subtractedNumbers = nr1 - nr2;
+        System.out.println("Die Zahlen " + nr1 + " und " + nr2 + " ergeben subtrahiert " + subtractedNumbers);
     }
 
-    public void multiply(){
-        System.out.println("Ich kann multiplizieren.");
+    public void multiply(int nr1, int nr2){
+        int multipliedNumbers = nr1 * nr2;
+        System.out.println("Die Zahlen " + nr1 + " und " + nr2 + " ergeben multipliziert " + multipliedNumbers);
     }
 
-    public void divide(){
-        System.out.println("Ich kann dividieren.");
+    public void divide(int nr1, int nr2){
+        int dividedNumbers = nr1 / nr2;
+        System.out.println("Die Zahlen " + nr1 + " und " + nr2 + " ergeben dividiert " + dividedNumbers);
     }
 }
