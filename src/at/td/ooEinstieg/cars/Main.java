9b7845c066e871dc513cc4e1f23f7790751ea54b
Theresa Denz <tdenz@gmx.at>
2022-01-19 09:18:49 +0100

    used newBaseConsumption() function

--- src/at/td/ooEinstieg/cars/Main.java
+++ src/at/td/ooEinstieg/cars/Main.java
@@ -9,8 +9,11 @@ public class Main {
         Producer p1 = new Producer("Opel", "Deutschland", 10);
         Producer p2 = new Producer("BMW", "Frankreich", 12);
 
-        Car c1 = new Car(e1,p1, "blue", 220, 34000, 5);
+        Car c1 = new Car(e1,p1, "blue", 220, 34000, 5, 20000);
+        Car c2 = new Car(e2, p2, "black", 200, 23000, 4, 65000);
 
         c1.discountedPrice();
+        c1.newBaseComsumption();
+        c2.newBaseComsumption();
     }
 }
