d7e406a32b5ef73b7a3a5ed527c5927e6f9a2cd8
Theresa Denz <tdenz@gmx.at>
2022-01-19 09:24:53 +0100

    created a basic calculator

---
+++ src/at/td/ooEinstieg/Calculator/Main.java
@@ -0,0 +1,9 @@
+package at.td.ooEinstieg.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        basicCalculator b1 = new basicCalculator("dark blue");
+
+        b1.subtract();
+    }
+}
