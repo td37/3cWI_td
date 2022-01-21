461a82a74d0376a02ce8f8bc10f545f450c7c99b
Theresa Denz <tdenz@gmx.at>
2022-01-21 11:26:22 +0100

    basic structure for inheritance

---
+++ src/at/td/ooEinstieg/geometricFigures/Figure.java
@@ -0,0 +1,19 @@
+package at.td.ooEinstieg.geometricFigures;
+
+public class Figure {
+    private String name;
+
+    public Figure(String name) {
+        this.name = name;
+    }
+
+    public void getArea(){    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
