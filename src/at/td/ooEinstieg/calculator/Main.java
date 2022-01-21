14126b1e9365c0902a2b489b0e2fecd1a0bc85fd
Theresa Denz <tdenz@gmx.at>
2022-01-21 11:49:36 +0100

    created basic, scientific and root calculator

--- src/at/td/ooEinstieg/calculator/Main.java
+++ src/at/td/ooEinstieg/calculator/Main.java
@@ -1,12 +1,18 @@
-package at.td.ooEinstieg.Calculator;
+package at.td.ooEinstieg.calculator;
 
 public class Main {
     public static void main(String[] args) {
         basicCalculator bC1 = new basicCalculator("dark blue");
-
         bC1.add(2, 5);
         bC1.subtract(2, 5);
         bC1.multiply(2, 5);
         bC1.divide(10, 5);
+
+        scientificCalculator sC1 = new scientificCalculator("green");
+        sC1.sin(60);
+        sC1.cos(40);
+
+        rootCalculator rC1 = new rootCalculator("balck");
+        rC1.root(1453);
     }
 }
