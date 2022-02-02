dea5f11e510134bfd8897f6feb27f65278bf5b05
Theresa Denz <tdenz@gmx.at>
2022-02-02 09:33:02 +0100

    function to calculate how much your account itself costs you

---
+++ src/at/td/ooEinstieg/bankAccount/LaendleAccount.java
@@ -0,0 +1,27 @@
+package at.td.ooEinstieg.bankAccount;
+
+public class LaendleAccount extends BaseAccount {
+    private double membershipYears;
+    private double accountManagement = 83.04;
+
+    public LaendleAccount(double balance, double membershipYears) {
+        super(balance);
+        this.membershipYears = membershipYears;
+    }
+
+    public void accountKeepingCosts(){
+        double accountKeepingCosts = 0;
+        for (int i = 0; i < getMembershipYears(); i++) {
+            accountKeepingCosts = accountKeepingCosts + this.accountManagement;
+        }
+        System.out.println("Dein Ländlegirokonto kostete dich seit deiner Kontoeröffnung " + accountKeepingCosts + "€");
+    }
+
+    public double getMembershipYears() {
+        return membershipYears;
+    }
+
+    public double getAccountManagement() {
+        return accountManagement;
+    }
+}
