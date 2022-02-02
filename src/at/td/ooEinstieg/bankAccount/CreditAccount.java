0bd2db66d9a99db673599f5d19cf1bebe40fb495
Theresa Denz <tdenz@gmx.at>
2022-02-02 09:31:47 +0100

    function to calculate if you have to pay credit costs

---
+++ src/at/td/ooEinstieg/bankAccount/CreditAccount.java
@@ -0,0 +1,38 @@
+package at.td.ooEinstieg.bankAccount;
+
+public class CreditAccount extends BaseAccount{
+    private double loanInterest = 0.023;
+    private double creditLimit = -200;
+    private double loanMonths;
+
+    public CreditAccount(double balance, double loanMonths) {
+        super(balance);
+        this.loanMonths = loanMonths;
+    }
+
+    public void creditCosts(){
+        double creditCosts = 0;
+        if(this.getBalance() <= this.creditLimit){
+            System.out.println("Du hast das Kontolimit überschritten.");
+        }
+        else if(this.getBalance() >= 0){
+            System.out.println("Du musst keine Kreditkosten abzahlen.");
+        }
+        else{
+            creditCosts = (-this.getBalance() * this.loanInterest) * this.loanMonths;
+            System.out.println("Du musst " + creditCosts + "€ Kreditzinsen zahlen.");
+        }
+    }
+
+    public double getLoanInterest() {
+        return loanInterest;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public double getLoanMonths() {
+        return loanMonths;
+    }
+}
