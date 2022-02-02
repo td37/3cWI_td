c9cf3aa5a531d4468153491db5ec4516f6d393bf
Theresa Denz <tdenz@gmx.at>
2022-02-02 09:28:50 +0100

    function to calculate the account interests

---
+++ src/at/td/ooEinstieg/bankAccount/SavingsAccount.java
@@ -0,0 +1,26 @@
+package at.td.ooEinstieg.bankAccount;
+
+public class SavingsAccount extends BaseAccount{
+
+    private double savingYears;
+    private double rateOfInterest;
+
+    public SavingsAccount(double balance, double savingYears, double rateOfInterest) {
+        super(balance);
+        this.savingYears = savingYears;
+        this.rateOfInterest = rateOfInterest;
+    }
+
+    public void accountInterest(){
+        double accountInterest = (this.getBalance() * this.rateOfInterest) * this.savingYears;
+        System.out.println("Du hast in den letzten " + this.savingYears + " Jahren " + accountInterest + "€ Zinsen erhalten.");
+    }
+
+    public double getSavingYears() {
+        return savingYears;
+    }
+
+    public double getRateOfInterest() {
+        return rateOfInterest;
+    }
+}
